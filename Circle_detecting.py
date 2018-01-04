0279a60edba84975cf7b54dc80d39be95e25698a
alpergenc <alpergenc@users.noreply.github.com>
2018-01-04 23:44:34 +0300

    finished

--- Circle_detecting.py
+++ Circle_detecting.py
@@ -1,33 +1,31 @@
-
 import cv2
 import numpy as np
 
-
-
-
+# Daha duzgun cember bulma algoritmasi
+# A better circle detecting algorithm
 
 # TO DO LIST:
 
-# 1. Update kisminda problem var. Duzgun update edilemiyor.
-# 2. Update edildikten sonra circle cizilmesi gerek
-# 3. Siralama degismemesi lazim
+# nothing here!
 
-
-def close(a, b, thr):
+def close(a, b, thr):       # close check
     if abs(a - b) < thr:
         return 1
     return 0
 
+eski_c=0
 
 diz = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 old_diz = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+fin_diz = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
 print "BASLANGIC"
 cap = cv2.VideoCapture("test_vid.mp4")
-thr = 10
+thr = 10  # yakinlik derecesi
 
-fr = 0
+fr = 1  # bool first run
 while 1:
-
+    nc_c=0
     circles = 0
     print "detecting"
     while 1:
@@ -39,7 +37,7 @@ while 1:
         cimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         hsv = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2HSV)
 
-        circles = cv2.HoughCircles(cimg, cv2.HOUGH_GRADIENT, 1.1, 85, param1=30, param2=70, minRadius=52,
+        circles = cv2.HoughCircles(cimg, cv2.HOUGH_GRADIENT, 1.1, 85, param1=30, param2=70, minRadius=50,
                                    maxRadius=75)  # 90
 
         # Param 1 will set the sensitivity; how strong the edges of the circles need to be. Too high and it won't detect anything, too low and it will find too much clutter.
@@ -50,41 +48,62 @@ while 1:
             break
 
     c = 0
-    sayac = 0
-    sayac2 = 0
+    print "yeniler:"
     for i in circles[0, :]:
-        # draw the outer circle
+
 
         diz[c] = i
-        if fr == 0:
+        if fr == 1:  # if first run
             print "only once"
             old_diz[c] = i
-
+            fin_diz[c] = i
+            eski_c=c
         print diz[c]
 
         c += 1
+
+
     sayac = 0
-    fr = 1
+    fr = 0   # not first run anymore
+
     print "Eski dizi"
-    while (sayac < c):
+    while (sayac < eski_c):
         print old_diz[sayac]
 
         sayac += 1
 
-    print " "
     sayac = 0
     sayac2 = 0
+
+    while sayac2 < c:
+        while sayac < eski_c:
+            if (close(old_diz[sayac][0], diz[sayac2][0], thr) and close(old_diz[sayac][1], diz[sayac2][1], thr)):
+                # update
+                nc_c=1               # not close counter
+
+
+            sayac += 1
+        if (nc_c == 0):
+            print "eslesme yok"
+            print diz[sayac2][:]
+            fin_diz[sayac2] = diz[sayac2]
+        nc_c=0
+        sayac2 += 1
+        sayac = 0
+
+    print " "
+
+    sayac = 0
+    sayac2 = 0
+
     while sayac < c:
-        while sayac2 < c:
-            print ""
-            print old_diz[sayac2][0]
-            print "vs"
-            print diz[sayac][0]
+        while sayac2 < eski_c:
             if (close(old_diz[sayac2][0], diz[sayac][0], thr) and close(old_diz[sayac2][1], diz[sayac][1], thr)) and (
                 diz[sayac][2] > old_diz[sayac2][2]):
                 # update
-                print "update new: ", old_diz[sayac2][0],"old: ", diz[sayac][0]
-                diz[sayac][:] = old_diz[sayac2][:]
+                print "update new: ", diz[sayac][0],"old: ", old_diz[sayac2][0]
+                print "update new: ", diz[sayac][2], "old: ", old_diz[sayac2][2]
+                fin_diz[sayac2][:] = diz[sayac][:]
 
 
 
@@ -93,27 +112,35 @@ while 1:
         sayac2 = 0
 
     sayac = 0
-    sayac2 = 0
+#
     while (sayac < c):
-        old_diz[sayac] = diz[sayac]
+         old_diz[sayac] = fin_diz[sayac]
 
-        sayac += 1
+         sayac += 1
+
+
     sayac = 0
+    print "count",c
     print "Yenilenen dizi"
     while (sayac < c):
-        print diz[sayac]
-        cv2.circle(imgOriginal, (diz[sayac][0], diz[sayac][1]), diz[sayac][2], (0, 255, 0), 2)
+        print fin_diz[sayac]
+        # draw the outer circle
+        cv2.circle(imgOriginal, (fin_diz[sayac][0], fin_diz[sayac][1]), fin_diz[sayac][2], (0, 255, 0), 2)
         # draw the center of the circle
-        cv2.circle(imgOriginal, (diz[sayac][0], diz[sayac][1]), 2, (0, 0, 255), 3)
+        cv2.circle(imgOriginal, (fin_diz[sayac][0], fin_diz[sayac][1]), 2, (0, 0, 255), 3)
         sayac += 1
-
+    eski_c=c
     print " "
     font = cv2.FONT_HERSHEY_SIMPLEX
     yenicount = 0
     while yenicount < c:
         cv2.putText(imgOriginal,
-                    str(int(diz[yenicount][0])) + "," + str(int(diz[yenicount][1])) + "," + str(int(diz[yenicount][2])),
-                    (diz[yenicount][0], diz[yenicount][1]), font, 2,
+                    str(int(fin_diz[yenicount][0])) + "," + str(int(fin_diz[yenicount][1])) + "," + str(int(fin_diz[yenicount][2])),
+                    (fin_diz[yenicount][0], fin_diz[yenicount][1]), font, 2,
+                    (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(imgOriginal,
+                    str(int(fin_diz[yenicount][0])),
+                    (800,yenicount*70+50), font, 2,
                     (255, 255, 255), 2, cv2.LINE_AA)
         yenicount += 1
 
